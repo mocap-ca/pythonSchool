216836a5ec2ce77bb4e56182575d047ec21af6ee
al <al@mocap.ca>
2020-08-07 10:43:51 -0700

    added labels, made them as class members

--- class1/simpleGui.py
+++ class1/simpleGui.py
@@ -6,13 +6,23 @@ class MyDialog(QtWidgets.QDialog):
     def __init__(self, parent=None):
         super(MyDialog, self).__init__(parent)
         layout = QtWidgets.QHBoxLayout()
-        label = QtWidgets.QLabel("Hello")
-        layout.addWidget(label)
+        self.label1 = QtWidgets.QLabel("Hello Label 1")
+        self.label2 = QtWidgets.QLabel("Hello Label 2")
+        self.label3 = QtWidgets.QLabel("Hello Label 3")
+        layout.addWidget(self.label1)
+        layout.addWidget(self.label2)
+        layout.addWidget(self.label3)
         self.setLayout(layout)
         self.resize(400, 400)
 
 
-if __name__ == "__main__":
+def show_dialog():
     app = QtWidgets.QApplication(sys.argv)
     d = MyDialog()
-    d.exec_()
+    d.label1.setText("HELLO!!!")
+    print(d.label1.text())
+    d.exec_()  # blocking call
+
+
+if __name__ == "__main__":
+    show_dialog()
