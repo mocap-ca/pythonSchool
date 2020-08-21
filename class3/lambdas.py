44f1650d11415ff5ebb2fc201ed5be9c5c814d2d
Alastair Macleod mini home <al@mocap.ca>
2020-08-21 12:31:08 -0700

    class2 content

--- class3/lambdas.py
+++ class3/lambdas.py
@@ -11,13 +11,23 @@ class MyDialog(QtWidgets.QDialog):
         super(MyDialog, self).__init__(parent)
         layout = QtWidgets.QHBoxLayout()
         self.cb1 = QtWidgets.QCheckBox("One")
+        self.cb1.stateChanged.connect(partial(self.checkbox_event, "ONE"))
         self.cb2 = QtWidgets.QCheckBox("Two")
+        self.cb2.stateChanged.connect(self.checkbox_event)
         self.cb3 = QtWidgets.QCheckBox("Three")
+        self.cb3.stateChanged.connect(self.checkbox_event)
         layout.addWidget(self.cb1)
         layout.addWidget(self.cb2)
         layout.addWidget(self.cb3)
         self.setLayout(layout)
         self.resize(400, 400)
+
+    def checkbox_event(self, ref, value):
+
+        if value & QtCore.Qt.Checked:
+            print("Checked!  " + str(ref))
+        else:
+            print("Unchecked")
 
 def show_dialog():
     app = QtWidgets.QApplication(sys.argv)
