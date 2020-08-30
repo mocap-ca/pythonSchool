3fd2836ae70f121d8ca194d9817313b201147975
Alastair Macleod mini home <al@mocap.ca>
2020-08-30 11:26:48 -0700

    added hello file in class1

---
+++ class1/hello.py
@@ -0,0 +1,19 @@
+
+"""
+CLASS WILL BE STARTING SOON
+
+Feel free to ask questions or post in the discord general channel.
+
+"""
+
+def function(arg):
+    a = 1
+    b = "value"
+    if a is 1:
+        print("A is One")
+
+
+
+if __name__ == "__main__":
+    function(None)
+
