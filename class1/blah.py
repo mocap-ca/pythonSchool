21387ced56025995525c4986bfd445061588a794
Alastair Macleod mini home <al@mocap.ca>
2020-08-30 10:41:48 -0700

    class 1 duck typing

--- class1/blah.py
+++ class1/blah.py
@@ -1,4 +1,32 @@
-import simpleGui
 
-if __name__ == "__main__":
-    print("hi")
+class Foo(object):
+    def __str__(self):
+        return "Foo!"
+    pass
+
+
+class Bloop():
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        ret = "I am a bloop, my value is: "
+        if isinstance(self.value, float):
+            return ret + "%.04f" % self.value
+        if isinstance(self.value, int):
+            return ret + "%d" % self.value
+
+        return ret + " string value of: " + str(self.value)
+
+
+items = [ "Apple", 0.4,  2, "Carrot",  Foo,  Foo() ]
+
+
+for i in items:
+    print(str(i))
+
+
+
+
+
