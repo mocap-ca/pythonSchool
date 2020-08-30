8a347bc9f27ef8ec2d42ff60dad76bcf50f7ec71
Alastair Macleod mini home <al@mocap.ca>
2020-08-30 11:12:23 -0700

    lambda example revisit

---
+++ class4/lambdas.py
@@ -0,0 +1,15 @@
+
+def val(a):
+    return int(a)
+
+a = ["100", "4", "11", "2", "15", "6"]
+
+for i in sorted(a, key=val):
+    print(i)
+
+for i in sorted(a, key=lambda v: int(v)):
+    print(i)
+
+
+print(lambda v: int(v))
+
