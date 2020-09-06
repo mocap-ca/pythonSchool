7b154bde0db63946b2651d641c2662d5fdcb8cb3
DJ-TMC <60460753+DJ-TMC@users.noreply.github.com>
2020-09-06 10:44:52 -0700

    made requested changes

--- assignment1/hello_dj_tmc.py
+++ assignment1/hello_dj_tmc.py
@@ -1,3 +1,6 @@
-print("Hello, World!")
-print("Test commit 2")
-print("test commit 3")
+if __name__ == "__main__":
+    print("Hello, World!")
+    print("test commit 2")
+    print("test commit 3")
+
+
