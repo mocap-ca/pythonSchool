1c5a885bbd618e150471662cb4387177fe9c5035
BananaFeet <himi86@gmail.com>
2020-09-12 21:58:10 -0700

    Updated maya window GUI to include widgets. widgets are not functional yet

--- assignment1/michelle.py
+++ assignment1/michelle.py
@@ -4,37 +4,42 @@ from PySide2 import QtWidgets
 
 
 
-class RigWin(QtWidgets.QDialog):
+class ImportWin(QtWidgets.QDialog):
     def __init__(self, parent=None):
-        super(RigWin, self).__init__(parent)
-
+        super(ImportWin, self).__init__(parent)
         '''Window details. Setting window name and minimum size'''
-        self.setWindowTitle("Rigging Tools")
-        self.setMinimumSize(400)
+        self.setWindowTitle("Import")
+        self.resize(400,400)
 
-        self.create_widget()
-        self.create_layout()
+        '''Create widgets'''
+        self.dir_path = QtWidgets.QLineEdit()
+        self.browse_button = QtWidgets.QPushButton("Browse Path")
+        #self.import_button = QtWidgets.QPushButton("Import files")
 
-        '''Creating all widgets under a function'''
-        def create_widget(self):
-            self.button_one = QtWidgets.QPushButton("Push Me")
 
-        '''Parents widgets from create_widget function to layout. 
-        Widgets need to be parented to the layout in order for them to
-        be visible and added to the window'''
-        def create_layout(self):
-            layout = QtWidgets.QVBoxLayout()
-            layout.addWidget(self.button_one)
+        '''Create label for widgets'''
 
-        #
-        #
-        # layout.addWidget(self.button_one)
-        #
-        # self.layout()
+        '''Set layout for widgets'''
+        self.layout = QtWidgets.QHBoxLayout()
+
+        '''Create event for widgets when clicked'''
+        self.browse_button.clicked.connect(self.browse_path)
+
+        '''Adding widgets and setting the layout'''
+        self.layout.addWidget(self.dir_path)
+        self.layout.addWidget(self.browse_button)
+        #self.layout.addWidget(self.import_button)
+        self.setLayout(self.layout)
+
+    def browse_path(self):
+        '''When browse button is pushed, select directory path to browse
+        Once path is selected and set, FBX items only will be listed'''
+        print("Hello!")
+
 
 
 
 
 if __name__ == "__main__": # this is when your program start
-    d = RigWin()
+    d = ImportWin()
     d.show()
