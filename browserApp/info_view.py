7695b22e54e4160cd80e2be3bda14fe6149a6817
megmugur <jois.megha@gmail.com>
2020-11-08 09:56:37 -0800

    adding RMB context menu, wip

--- browserApp/info_view.py
+++ browserApp/info_view.py
@@ -7,7 +7,7 @@ Assumptions made: file and folder names doe not contain spaces or dots. Only one
 3. Layout width is fixed to make it uniform. Make it scalable.
 4. Allow folders and files with spaces in the name
 5. Allow folders and files with dots in the name? Not sure. Maybe.
-6. Add a small line between the header cells in the details table, so that the user can widen the columns if needed.
+6. Add a separator in-between the header cells in the details table, so that the user can widen the columns if needed.
 7. Remove bold font for selected column in header
 8. Create buttons only for the selected View type, and not for all views.
 9. Request more data from model. Refer to inline comments.
@@ -15,14 +15,14 @@ Assumptions made: file and folder names doe not contain spaces or dots. Only one
 """
 
 try:
-    from PySide2 import QtWidgets, QtCore
+    from PySide2 import QtWidgets, QtCore, QtGui
     create_signal = QtCore.Signal
 except:
-    from PyQt5 import QtWidgets, QtCore
+    from PyQt5 import QtWidgets, QtCore, QtGui
     create_signal = QtCore.pyqtSignal
 
 import datetime
-from os import listdir, system, path, stat
+from os import listdir, system, path, remove, rename
 from os.path import isfile, join
 from functools import partial
 import model.file
@@ -66,18 +66,19 @@ class InfoView(QtWidgets.QWidget):
         self.icons_row_index = 0
         self.NUMBER_OF_GRID_COLUMNS = 2
 
-        self.details_header_list = ["Name", "Date", "Type", "Size"]                        # Is this a string constant??
+        self.details_header_list = ["Name", "Date", "Type", "Size"]                   # Is this a string constant list??
         self.details_row_index = 0
         self.details_column_index = 0
         self.data = {}
         self.icons_filename_buttons = {}
         self.details_filename_buttons = {}
+        self.buttons_dictionary = {}
 
         self.setLayout(layout)
         self.setMinimumWidth(520)
 
     def populate(self, data):
-        """Populates the Display area of the window with a stacked widget. The stacked widgets contains two pages:
+        """ Populates the Display area of the window with a stacked widget. The stacked widgets contains two pages:
         the Icons View page which shows the contents of the folder as a grid of buttons, and
         the Details View page which shows a table of files(buttons) and their details, eg., file size, date modified.
         Todo: Get file details from meta
@@ -87,11 +88,10 @@ class InfoView(QtWidgets.QWidget):
         self.data = data
         meta = data.get_info()
         print("Meta: ", meta)
-        if 'full_path' in meta:
-            item_path = meta['full_path']
-        else:
+        if 'full_path' not in meta:
             print("Could not find file path in dictionary")
             return
+        item_path = meta['full_path']
 
         # Clear List View:
         clear_table_widget(self.details_widget)
@@ -99,39 +99,41 @@ class InfoView(QtWidgets.QWidget):
         # Clear Icons View:
         clear_grid_layout(self.icons_layout)
         self.reset_row_column_counts()
+        # self.add_view_options_buttons(self.icons_layout)
 
-        if 'type' in meta:
-            # File:
-            if meta['type'] == 'File':          # If we remove files from the collection view, we can delete this check.
-                pass
+        if 'type' not in meta:
+            print("Could not find item type (file or folder) in dictionary.")
+            return
 
-            # Directory:
-            elif meta['type'] == 'Dir':
-                self.icons_layout.setAlignment(QtCore.Qt.AlignTop)
-                files = []
-                directories = []
-                files_and_folders = listdir(item_path)
-                print(files_and_folders)
-                for item in files_and_folders:
-                    if isfile(join(item_path, item)):                   # Request addition of 'file_name' to data model.
-                        files.append(item)
-                    else:
-                        directories.append(item)
+        # File:
+        if meta['type'] == 'File':          # If we remove files from the collection view, we can delete this check.
+            return
 
-                self.reset_row_column_counts()
+        # Directory:
+        self.icons_layout.setAlignment(QtCore.Qt.AlignTop)
+        files = []
+        directories = []
+        files_and_folders = listdir(item_path)
+        print(files_and_folders)
+        for item in files_and_folders:
+            if isfile(join(item_path, item)):                   # Request addition of 'file_name' to data model.
+                files.append(item)
+            else:
+                directories.append(item)
 
-                self.icons_widget.setLayout(self.icons_layout)
-                self.icons_layout.setSpacing(0)                       # sets spacing between widgets in the layout to 0.
+        self.reset_row_column_counts()
 
-                for item in files_and_folders:
-                    self.add_item_to_detailsview(item, item_path)
-                    self.add_item_to_iconview(item, item_path)
+        self.icons_widget.setLayout(self.icons_layout)
+        self.icons_layout.setSpacing(0)                       # sets spacing between widgets in the layout to 0.
 
-        else:
-            print("Could not find file type in dictionary")
+        for item in files_and_folders:
+            self.add_item_to_detailsview(item, item_path)
+            self.add_item_to_iconview(item, item_path)
+
+
 
     def reset_row_column_counts(self):
-        """ Resets the row and column indices to zero."""
+        """ Resets the row and column indices to zero. """
         self.details_row_index = 0
         self.details_column_index = 0
         self.icons_column_index = 0
@@ -268,7 +270,73 @@ class InfoView(QtWidgets.QWidget):
         button = QtWidgets.QPushButton(button_name)
         button.clicked.connect(partial(self.on_item_clicked, file_path))
         set_button_style(button)
+
+        if not isfile(file_path):
+            return button
+        # set button context menu policy
+        button.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        button.customContextMenuRequested.connect(partial(self.show_rightclick_menu, button))
+        self.buttons_dictionary[button] = file_path
+
         return button
+
+    def show_rightclick_menu(self, button, point):
+        """ Displays a menu on right mouse button click.
+        :param button: button user clicks on.
+        :type button: QPushButton
+        :param point: ??
+        :type point: QtCore.QPoint
+        """
+
+        # create context menu
+        pop_menu = QtWidgets.QMenu(self)
+
+        delete_action = QtWidgets.QAction('Delete', self)
+        pop_menu.addAction(delete_action)
+        delete_action.triggered.connect(partial(self.delete_item, button))
+
+        open_action = QtWidgets.QAction('Open', self)
+        pop_menu.addAction(open_action)
+        open_action.triggered.connect(partial(self.open_item, button))
+
+        # rename_action = QtWidgets.QAction('Rename', self)
+        # pop_menu.addAction(rename_action)
+        # rename_action.triggered.connect(partial(self.rename_item, button))
+
+        # show context menu
+        pop_menu.exec_(button.mapToGlobal(point))
+
+    def delete_item(self, button):
+        """ Deletes the selected file.
+        :param button: button user clicks on.
+        :type button: QPushButton
+        """
+        if path.exists(self.buttons_dictionary[button]):
+            remove(self.buttons_dictionary[button])
+        print("Deleted:", self.buttons_dictionary[button])
+
+        # refresh view
+        self.refresh_details_view()
+
+    def refresh_details_view(self):
+        """ Refreshes the view. """
+        pass                                                                                       # Todo
+
+    def open_item(self, button):
+        """ Opens the selected file.
+        :param button: button user clicks on.
+        :type button: QPushButton
+        """
+        file_path = self.buttons_dictionary[button]
+        if path.exists(file_path):
+            self.on_item_clicked(file_path)
+
+    # def rename_item(self, button):
+    #     file_path = self.buttons_dictionary[button]
+    #
+    #     new_file_path = "some new name"
+    #     if path.exists(file_path):
+    #         rename(file_path, new_file_path)
 
 
 def create_table_widget_header(widget, header_items):
@@ -289,7 +357,7 @@ def add_icon_to_button(button, file_path):
     :type button: QPushButton
     :param file_path: full path to the file whose icon is to be added to the button
     :type file_path: str"""
-
+    print(type(button))
     file_info = QtCore.QFileInfo(file_path)
     icon_provider = QtWidgets.QFileIconProvider()
     icon = icon_provider.icon(file_info)
@@ -316,6 +384,12 @@ def clear_grid_layout(grid_layout):
             child.widget().deleteLater()
 
 
+# def add_view_options_buttons(grid_layout):
+#     details_button = QtWidgets.QPushButton('SP_FileDialogInfoView')
+#     icons_button = QtWidgets.QPushButton('SP_FileDialogListView')
+#     details_button.setIcon(self.style().standardIcon(getattr(QStyle, 'SP_FileDialogInfoView')))
+#     icons_button.setIcon(self.style().standardIcon(getattr(QStyle, 'SP_FileDialogListView')))
+
 def set_button_style(button):
     """ Sets the button style and dimensions. No background or border by default. Turns blue with a border on hover.
     :param button: the buttons with the file name and icon on them.
@@ -341,6 +415,25 @@ def convert_filesize_to_str(size_long):
         return str(size_long / (10 ** 6)) + " MB"
     else:
         return str(size_long / (10 ** 9)) + " GB"
+
+
+# class QDoublePushButton(QtWidgets.QPushButton):
+#     doubleClicked = QtCore.Signal()
+#     clicked = QtCore.Signal()
+#
+#     def __init__(self, *args, **kwargs):
+#         QtWidgets.QPushButton.__init__(self, *args, **kwargs)
+#         self.timer = QtWidgets.QTimer()
+#         self.timer.setSingleShot(True)
+#         self.timer.timeout.connect(self.clicked.emit)
+#         super().clicked.connect(self.check_double_click)
+#
+#     def check_double_click(self):
+#         if self.timer.isActive():
+#             self.doubleClicked.emit()
+#             self.timer.stop()
+#         else:
+#             self.timer.start(250)
 
 
 
